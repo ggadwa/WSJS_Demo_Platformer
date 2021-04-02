525e01376e5eb7a8f30c061a307b47b00a66ae0e
Brian Barnes <ggadwa@charter.net>
2021-04-02 01:55:32 -0400

    04_02_2021 Script cont

--- effects/flash.js
+++ effects/flash.js
@@ -1,4 +1,5 @@
 import PointClass from '../../../code/utility/point.js';
+import ColorClass from '../../../code/utility/color.js';
 import EffectClass from '../../../code/game/effect.js';
 
 export default class FlashClass extends EffectClass
@@ -7,20 +8,13 @@ export default class FlashClass extends EffectClass
     {
         super(core,spawnedBy,position,data,mapSpawn,show);
         
-        this.lifeTick=1500;
-        this.billboards=
-            [
-                {
-                    "bitmap":"textures/particle_hit.png","mode":"transparent",
-                    "frames":
-                        [
-                            {"tick":0,"width":10,"height":10,"rotate":0,"color":{"r":1.0,"g":1.0,"b":0.0},"alpha":0.3},
-                            {"tick":100,"width":10000,"height":10000,"rotate":30,"color":{"r":1.0,"g":0.7,"b":0.0},"alpha":1.0},
-                            {"tick":800,"width":15000,"height":15000,"rotate":90,"color":{"r":1.0,"g":0.6,"b":0.0},"alpha":1.0},
-                            {"tick":1500,"width":500,"height":500,"rotate":180,"color":{"r":0.7,"g":0.5,"b":0.5},"alpha":0.1}
-                        ]
-                }
-            ];
+        this.lifeTick=1400;
+        
+        this.addBillboard("textures/particle_hit.png",this.DRAW_MODE_TRANSPARENT)
+                .addBillboardFrame(0,10000,10000,0,new ColorClass(1.0,1.0,0.0),0.3)
+                .addBillboardFrame(10,10000,10000,30,new ColorClass(1.0,0.7,0.0),1.0)
+                .addBillboardFrame(800,15000,15000,90,new ColorClass(1.0,0.6,0.0),1.0)
+                .addBillboardFrame(1400,50,50,180,new ColorClass(0.7,0.5,0.5),0.2);
     }
 
 }
--- effects/warp.js
+++ effects/warp.js
@@ -1,4 +1,5 @@
 import PointClass from '../../../code/utility/point.js';
+import ColorClass from '../../../code/utility/color.js';
 import EffectClass from '../../../code/game/effect.js';
 
 export default class FlashClass extends EffectClass
@@ -7,21 +8,15 @@ export default class FlashClass extends EffectClass
     {
         super(core,spawnedBy,position,data,mapSpawn,show);
    
-        this.lifeTick=1000;
-        this.billboards=
-            [
-                {
-                    "bitmap":"textures/particle_hit.png","mode":"transparent",
-                    "frames":
-                        [
-                            {"tick":0,"width":10,"height":100,"rotate":0,"color":{"r":1.0,"g":1.0,"b":1.0},"alpha":0.3},
-                            {"tick":100,"width":10000,"height":15000,"rotate":30,"color":{"r":0.9,"g":0.9,"b":0.95},"alpha":1.0},
-                            {"tick":800,"width":15000,"height":25000,"rotate":90,"color":{"r":0.7,"g":0.7,"b":0.9},"alpha":1.0},
-                            {"tick":1500,"width":500,"height":800,"rotate":180,"color":{"r":0.5,"g":0.5,"b":0.85},"alpha":0.1}
-                        ]
-                }
-            ];
+        this.lifeTick=1500;
         
+        this.addBillboard("textures/particle_hit.png",this.DRAW_MODE_ADDITIVE)
+                .addBillboardFrame(0,100,100,0,new ColorClass(1.0,1.0,1.0),0.3)
+                .addBillboardFrame(50,10000,15000,5,new ColorClass(0.9,0.9,0.95),1.0)
+                .addBillboardFrame(50,12500,20000,0,new ColorClass(0.9,0.9,0.95),1.0)
+                .addBillboardFrame(800,15000,25000,-5,new ColorClass(0.7,0.7,0.9),1.0)
+                .addBillboardFrame(1500,500,500,0,new ColorClass(0.5,0.5,0.85),0.1);
+
         this.startSound={"name":"teleport","rate":1.0,"randomRateAdd":0,"distance":50000,"loopStart":0,"loopEnd":0,"loop":false};
     }
     
